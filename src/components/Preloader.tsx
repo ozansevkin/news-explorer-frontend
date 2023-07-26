2d8676d576e6a89811bc1c598e5775f7a2d3d90a
Ozan Sevkin <sevkinozan@gmail.com>
2023-07-26 20:29:14 +1000

    refactor: change icon import name

--- src/components/Preloader.tsx
+++ src/components/Preloader.tsx
@@ -1,10 +1,10 @@
 import Image from "next/image";
-import preloader from "@/images/icons/preloader.png";
+import preloaderIcon from "@/images/icons/preloader.png";
 
 export default function Preloader({ text }: { text: string }) {
   return (
     <div className="flex flex-col items-center justify-center py-20 gap-y-6">
-      <Image src={preloader} alt="preloader" className="animate-spin" />
+      <Image src={preloaderIcon} alt="preloader" className="animate-spin" />
       <p className="text-gray-400">{text}</p>
     </div>
   );
