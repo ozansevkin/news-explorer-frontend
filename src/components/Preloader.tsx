3862faf3ecf5a20dfc5b9b78f8d65c09ecb8782c
Ozan Sevkin <sevkinozan@gmail.com>
2023-07-28 21:47:38 +1000

    refactor: update preloader

--- src/components/Preloader.tsx
+++ src/components/Preloader.tsx
@@ -1,10 +1,7 @@
-import Image from "next/image";
-import preloaderIcon from "@/images/icons/preloader.png";
-
 export default function Preloader({ text }: { text: string }) {
   return (
     <div className="bg-neutral-100 flex flex-col items-center justify-center py-12 lg:py-20  gap-y-4 lg:gap-y-6">
-      <Image src={preloaderIcon} alt="preloader" className="animate-spin" />
+      <span className="w-[74px] h-[74px] rounded-full border-2 border-gray-300 border-b-gray-800 animate-spin" />
       <p className="text-gray-400">{text}</p>
     </div>
   );
