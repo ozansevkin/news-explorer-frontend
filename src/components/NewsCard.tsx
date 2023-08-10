3cd29ea4cabf066abdcfc519fe11df3a9f575a47
Ozan Sevkin <sevkinozan@gmail.com>
2023-08-10 15:24:16 +1000

    fix: use fixed heights and manage overflow to match design

--- src/components/NewsCard.tsx
+++ src/components/NewsCard.tsx
@@ -1,7 +1,7 @@
 import { MouseEvent } from "react";
 import BookmarkIcon from "./icons/BookmarkIcon";
 import TrashIcon from "./icons/TrashIcon";
-import cardPlaceholder from "@/images/placeholder.svg";
+import cardPlaceholder from "@/images/placeholder.png";
 
 interface NewsCardProps {
   newsItem: newsItem;
@@ -38,10 +38,13 @@ export default function NewsCard({ newsItem }: NewsCardProps) {
   return (
     <article className="bg-white max-w-[400px] relative flex flex-col rounded-lg overflow-hidden hover:shadow-lg">
       {/* Next.js Image Component is not suitable to use here since we can not know all remote urls*/}
-      <img
-        src={urlToImage ? urlToImage : cardPlaceholder.src}
-        alt="News article"
-      />
+      <div>
+        <img
+          src={urlToImage ? urlToImage : cardPlaceholder.src}
+          alt="News article"
+          className="w-full h-[196px] sm:h-[150px] lg:h-[272px] object-fill"
+        />
+      </div>
       <button
         type="button"
         onClick={handleButtonClick}
@@ -58,22 +61,26 @@ export default function NewsCard({ newsItem }: NewsCardProps) {
       {/* <p className="absolute top-4 sm:top-2 lg:top-6 left-4 sm:left-2 lg:left-6 flex items-center justify-center rounded-lg bg-white h-10 px-[22px] text-black text-sm font-medium">
         Nature
       </p> */}
-      <a href={url} target="_blank">
-        <div className="flex flex-col p-4 lg:px-6 lg:pb-6 lg:pt-5">
+      <a
+        href={url}
+        target="_blank"
+        className="h-[244px] sm:h-[270px] lg:h-[304px]"
+      >
+        <div className="flex flex-col p-4 lg:px-6 lg:pb-6 lg:pt-5 h-full">
           <time
             dateTime="2018-07-07"
-            className="font-source-sans-3 text-gray-400"
+            className="font-source-sans-3 leading-6 text-gray-400"
           >
             {date}
           </time>
-          <h3 className="font-serif text-[22px] lg:text-[26px] leading-normal lg:leading-[30px] mt-[10px] sm:mt-3">
+          <h3 className="h-12 sm:h-[72px] lg:h-[60px] line-clamp-2 sm:line-clamp-3 lg:line-clamp-2 font-serif text-[22px] lg:text-[26px] leading-6 lg:leading-[30px] mt-[10px] sm:mt-3">
             {title}
           </h3>
-          <p className="text-base leading-snug mt-[14px] sm:mt-4">
+          <p className="line-clamp-3 sm:line-clamp-4 lg:line-clamp-5 text-base leading-snug mt-[14px] sm:mt-[10px] lg:mt-4">
             {description}
           </p>
-          <footer className="font-serif text-gray-400 text-base font-bold uppercase leading-tight tracking-wide [text-shadow:_0_4px_4px_rgb(0_0_0_/_0.25)] mt-2 sm:mt-3 lg:mt-[18px]">
-            <p>{source?.name}</p>
+          <footer className="font-serif text-gray-400 text-base font-bold uppercase leading-tight tracking-wide [text-shadow:_0_4px_4px_rgb(0_0_0_/_0.25)] mt-auto pt-2 sm:pt-3 lg:pt-[18px]">
+            <p className="truncate">{source?.name}</p>
           </footer>
         </div>
       </a>
